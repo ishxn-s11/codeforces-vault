192949a6eb68b58894ec43c012c918fc252cce5e
Ishan Singh <ishan29.s2908@gmail.com>
2026-09-24 09:38:41 +0530

    Create sol.cpp

---
+++ 282A/sol.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main(){
+    int n;
+    int x=0;
+    
+    cin>>n;
+    
+    while(n--){
+        string s;
+        cin>>s;
+        
+        if(s[1]=='+') x++;
+        else x--;
+    }
+    
+    cout<<x<<endl;
+    
+    return 0;
+    
+}
